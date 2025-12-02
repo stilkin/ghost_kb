13dedd8e2c4b28971c6915fc82e1738c58bd314c
stilkin <servaas.tilkin@gmail.com>
2025-12-02 22:30:06 +0100

    CHANGE: increased version nr to 6 (1.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
