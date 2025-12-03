55d19c886a60f3a56e049891375507c35a285696
stilkin <servaas.tilkin@gmail.com>
2025-12-03 20:27:02 +0100

    CHANGE: updated version nr to 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.15"
+        versionCode = 16
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
