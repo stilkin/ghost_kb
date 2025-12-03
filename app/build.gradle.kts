f44c11a34a2b8e7a39398e0fc0e4a88a45bc5c28
stilkin <servaas.tilkin@gmail.com>
2025-12-03 20:15:55 +0100

    CHANGE: changed app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.12"
+        versionCode = 13
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
