4efbb124467b1f82a189fa18322b48a885bc26bf
stilkin <servaas.tilkin@gmail.com>
2025-12-03 14:02:37 +0100

    CHANGE: updated version nr to 8 (1.7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
