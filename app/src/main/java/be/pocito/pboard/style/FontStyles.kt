d66dab5d1d24dced2d46c0127540a3173f50c322
stilkin <servaas.tilkin@gmail.com>
2025-12-03 14:03:20 +0100

    FIX: fixed some missing unicode characters

--- app/src/main/java/be/pocito/pboard/style/FontStyles.kt
+++ app/src/main/java/be/pocito/pboard/style/FontStyles.kt
@@ -184,7 +184,7 @@ object FontStyles {
         's' to "🅂", 't' to "🅃", 'u' to "🅄", 'v' to "🅅", 'w' to "🅆", 'x' to "🅇",
         'y' to "🅈", 'z' to "🅉",
         'A' to "🄰", 'B' to "🄱", 'C' to "🄲", 'D' to "🄳", 'E' to "🄴", 'F' to "🄵",
-        'G' to "🄶", 'H' to "🄷", 'I' to "🄸", 'J' to "��", 'K' to "🄺", 'L' to "🄻",
+        'G' to "🄶", 'H' to "🄷", 'I' to "🄸", 'J' to "🄹", 'K' to "🄺", 'L' to "🄻",
         'M' to "🄼", 'N' to "🄽", 'O' to "🄾", 'P' to "🄿", 'Q' to "🅀", 'R' to "🅁",
         'S' to "🅂", 'T' to "🅃", 'U' to "🅄", 'V' to "🅅", 'W' to "🅆", 'X' to "🅇",
         'Y' to "🅈", 'Z' to "🅉"
@@ -198,7 +198,7 @@ object FontStyles {
         'a' to "🅰", 'b' to "🅱", 'c' to "🅲", 'd' to "🅳", 'e' to "🅴", 'f' to "🅵",
         'g' to "🅶", 'h' to "🅷", 'i' to "🅸", 'j' to "🅹", 'k' to "🅺", 'l' to "🅻",
         'm' to "🅼", 'n' to "🅽", 'o' to "🅾", 'p' to "🅿", 'q' to "🆀", 'r' to "🆁",
-        's' to "🆂", 't' to "🆃", 'u' to "��", 'v' to "🆅", 'w' to "🆆", 'x' to "🆇",
+        's' to "🆂", 't' to "🆃", 'u' to "🆄", 'v' to "🆅", 'w' to "🆆", 'x' to "🆇",
         'y' to "🆈", 'z' to "🆉",
         'A' to "🅰", 'B' to "🅱", 'C' to "🅲", 'D' to "🅳", 'E' to "🅴", 'F' to "🅵",
         'G' to "🅶", 'H' to "🅷", 'I' to "🅸", 'J' to "🅹", 'K' to "🅺", 'L' to "🅻",
