b6ec26bd691a6d530b7eb0edb8ee34875a9ba6f8
stilkin <servaas.tilkin@gmail.com>
2025-12-03 20:17:02 +0100

    FIX: renamed svg to xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.13"
+        versionCode = 14
+        versionName = "1.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
