0ff50b26203aabe8deacb5deffb4e7dc0464fc0a
stilkin <servaas.tilkin@gmail.com>
2025-12-03 20:55:55 +0100

    CHANGE: increased version nr to 1.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.18"
+        versionCode = 19
+        versionName = "1.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
