73cf4e3ccdef47556a24f42b634b9b6a09a83e8b
stilkin <servaas.tilkin@gmail.com>
2025-12-03 19:51:01 +0100

    CHANGE: some more layout rework

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.10"
+        versionCode = 11
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
