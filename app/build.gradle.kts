dbddd16a3d942a1b05e6644b59e748dad514b23c
stilkin <servaas.tilkin@gmail.com>
2025-12-03 19:29:51 +0100

    CHANGE: version number 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
