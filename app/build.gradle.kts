d97184c8ff1495dfd3eccb212fb725ac341c01f8
stilkin <servaas.tilkin@gmail.com>
2026-04-03 17:30:23 +0200

    CHANGE: layout changes to main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "be.pocito.pboard"
         minSdk = 30
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
